273b78b3e7f999f622dabe732408d99ec93f4578
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-21 12:11:36 +0000

    Upload New File

---
+++ Calculations/Calculator.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Calculations
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// Calculate the following sum 1/1 + 1/2 + 1/3 + ... + 1/n, where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double GetSumOne(int n)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/(1*2) - 1/(2*3) + 1/(3*4) + ... + (-1)^(n+1) / (n * (n + 1)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double GetSumTwo(int n)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/1^5 + 1/2^5 + 1/3^5 + ... + 1/n^5, where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double GetSumThree(int n)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/(3 * 3) + 1/(5 * 5) + 1/(7 * 7) + ... + 1/((2 * n + 1) * (2 * n + 1)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double GetSumFour(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calculate the following product
+        /// (1 + 1/(1 * 1)) * (1 + 1/(2 * 2)) * (1 + 1/(3 * 3)) * ... * (1 + 1/(n * n)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Product of elements.</returns>
+        public static double GetProductOne(int n)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Calculate the following sum
+        /// -1/3 + 1/5 - 1/7 + ... + (-1)^n / (2 * n + 1), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double GetSumFive(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calculate the following sum
+        /// 1!/1 + 2!/(1+1/2) + 3!/(1+1/2+1/3) + ... + n!/ (1+1/2+1/3+...+1/n), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double GetSumSix(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calculate the following sum
+        /// Sqrt(2 + Sqrt(2 + Sqrt(2 + ...) ) ), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double GetSumSeven(int n)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/sin(1) + 1/(sin(1)+sin(2)) + ...+  1/(sin(1)+sin(2)+...+sin(n)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double GetSumEight(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
