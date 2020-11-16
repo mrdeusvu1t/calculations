bdab749d4817a4f5073381496fb61c404bef0c8c
Yury Hryshanau <mr.deusvult@gmail.com>
2020-11-16 12:40:41 +0300

    Up

--- Calculations/Calculator.cs
+++ Calculations/Calculator.cs
@@ -1,72 +1,79 @@
-using System;
+﻿using System;
 
 namespace Calculations
 {
     public static class Calculator
     {
-        /// <summary>
-        /// Calculate the following sum 1/1 + 1/2 + 1/3 + ... + 1/n, where n > 0.
-        /// </summary>
-        /// <param name="n">Number of elements.</param>
-        /// <returns>Sum of elements.</returns>
         public static double GetSumOne(int n)
         {
-            throw new NotImplementedException();
-        }
-        
-        /// <summary>
-        /// Calculate the following sum
-        /// 1/(1*2) - 1/(2*3) + 1/(3*4) + ... + (-1)^(n+1) / (n * (n + 1)), where n > 0.
-        /// </summary>
-        /// <param name="n">Number of elements.</param>
-        /// <returns>Sum of elements.</returns>
-        public static double GetSumTwo(int n)
-        {
-            throw new NotImplementedException();
-        }
-        
-        /// <summary>
-        /// Calculate the following sum
-        /// 1/1^5 + 1/2^5 + 1/3^5 + ... + 1/n^5, where n > 0.
-        /// </summary>
-        /// <param name="n">Number of elements.</param>
-        /// <returns>Sum of elements.</returns>
-        public static double GetSumThree(int n)
-        {
-            throw new NotImplementedException();
-        }
-        
-        /// <summary>
-        /// Calculate the following sum
-        /// 1/(3 * 3) + 1/(5 * 5) + 1/(7 * 7) + ... + 1/((2 * n + 1) * (2 * n + 1)), where n > 0.
-        /// </summary>
-        /// <param name="n">Number of elements.</param>
-        /// <returns>Sum of elements.</returns>
-        public static double GetSumFour(int n)
-        {
-            throw new NotImplementedException();
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += 1d / i;
+            }
+
+            return sum;
         }
 
-        /// <summary>
-        /// Calculate the following product
-        /// (1 + 1/(1 * 1)) * (1 + 1/(2 * 2)) * (1 + 1/(3 * 3)) * ... * (1 + 1/(n * n)), where n > 0.
-        /// </summary>
-        /// <param name="n">Number of elements.</param>
-        /// <returns>Product of elements.</returns>
+        public static double GetSumTwo(int n)
+        {
+            double sum = 0d;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += Math.Pow(-1d, i + 1) / (i * (i + 1d));
+            }
+
+            return sum;
+        }
+
+        public static double GetSumThree(int n)
+        {
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += 1d / Math.Pow(i, 5);
+            }
+
+            return sum;
+        }
+
+        public static double GetSumFour(int n)
+        {
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += 1d / (((2d * i) + 1d) * ((2d * i) + 1d));
+            }
+
+            return sum;
+        }
+
         public static double GetProductOne(int n)
         {
-            throw new NotImplementedException();
+            double sum = 1d;
+
+            for (double i = 1d; i <= n; i++)
+            {
+                sum *= 1d + (1d / (i * i));
+            }
+
+            return sum;
         }
-        
-        /// <summary>
-        /// Calculate the following sum
-        /// -1/3 + 1/5 - 1/7 + ... + (-1)^n / (2 * n + 1), where n > 0.
-        /// </summary>
-        /// <param name="n">Number of elements.</param>
-        /// <returns>Sum of elements.</returns>
+
         public static double GetSumFive(int n)
         {
-            throw new NotImplementedException();
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += Math.Pow(-1d, i) / ((2d * i) + 1d);
+            }
+
+            return sum;
         }
 
         /// <summary>
@@ -77,20 +84,32 @@ namespace Calculations
         /// <returns>Sum of elements.</returns>
         public static double GetSumSix(int n)
         {
-            throw new NotImplementedException();
+            double sum = 0;
+            var f = 1d;
+            var s = 0d;
+
+            for (int i = 1; i <= n; i++)
+            {
+                f *= i;
+                s += 1d / i;
+                sum += f / s;
+            }
+
+            return sum;
         }
 
-        /// <summary>
-        /// Calculate the following sum
-        /// Sqrt(2 + Sqrt(2 + ... + Sqrt(2))), where n > 0.
-        /// </summary>
-        /// <param name="n">Number of elements.</param>
-        /// <returns>Sum of elements.</returns>
         public static double GetSumSeven(int n)
         {
-            throw new NotImplementedException();
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum = Math.Sqrt(2 + sum);
+            }
+
+            return sum;
         }
-        
+
         /// <summary>
         /// Calculate the following sum
         /// 1/sin(1) + 1/(sin(1)+sin(2)) + ...+  1/(sin(1)+sin(2)+...+sin(n)), where n > 0.
@@ -99,7 +118,16 @@ namespace Calculations
         /// <returns>Sum of elements.</returns>
         public static double GetSumEight(int n)
         {
-            throw new NotImplementedException();
+            double sum = 0;
+            double sin = 0;
+
+            for (var i = 1d; i <= n; i++)
+            {
+                sin += Math.Sin(Math.PI * i / 180);
+                sum += 1d / sin;
+            }
+
+            return sum;
         }
     }
 }
